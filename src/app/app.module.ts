4143fc6f091b287a250c8c7c4f51d94f43017a64
Nabin Jaiswal <nabinjaiswal@gmail.com>
2017-09-01 23:36:41 +0530

    created the header and book components and also added assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { WelcomeComponent } from './welcome/welcome.component';
+import { BookDetailsComponent } from './books/book-details/book-details.component';
+import { BookListsComponent } from './books/book-lists/book-lists.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    BookDetailsComponent,
+    BookListsComponent
   ],
   imports: [
     BrowserModule
