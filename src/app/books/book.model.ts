b25da47d22255cf4b1ae1c2639889c18a2e34062
Nabin Jaiswal <nabinjaiswal@gmail.com>
2017-09-02 17:59:23 +0530

    created Book model and organized data in an array of Books using Book Model

---
+++ src/app/books/book.model.ts
@@ -0,0 +1,5 @@
+export class Books {
+
+  constructor (public id: number, public name: string, public productCode: string, public releaseDate: string, public description: string, public author: string, public genre: string, public specifications: string, public inStock: boolean, public price: number, public starRating: number, public imageUrl: string) { }
+
+}
